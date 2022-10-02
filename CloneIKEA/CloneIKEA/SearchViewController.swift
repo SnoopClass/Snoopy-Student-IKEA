21157892a4aa77af17ac7a7c96236aa285bf122d
skycat0212 <mrmagnet777@gmail.com>
2022-10-02 15:47:32 +0900

    [Add] Search뷰컨트롤러 생성 (#1)

---
+++ CloneIKEA/CloneIKEA/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  CloneIKEA
+//
+//  Created by 김수진 on 2022/10/02.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
