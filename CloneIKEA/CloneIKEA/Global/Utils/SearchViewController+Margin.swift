91e7ac977c76c49c8bb91ff20591e8472d60af4c
skycat0212 <mrmagnet777@gmail.com>
2022-10-04 22:01:56 +0900

    [Add] TitleLabel 구현 (#6)

---
+++ CloneIKEA/CloneIKEA/Global/Utils/SearchViewController+Margin.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController+Margin.swift
+//  CloneIKEA
+//
+//  Created by 김수진 on 2022/10/04.
+//
+
+import UIKit
+
+extension SearchViewController {
+    
+    enum Margin {
+        static let top = 30
+        static let sectionTitleLeft = 20
+    }
+    
+}
--- CloneIKEA/CloneIKEA/Screen/Search/Component/TitleSectionView.swift
+++ CloneIKEA/CloneIKEA/Screen/Search/Component/TitleSectionView.swift
@@ -11,6 +11,31 @@ extension SearchViewController {
     
     class TitleSectionView: UIView {
         
+        lazy var titleLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "검색"
+            label.font = .preferredFont(forTextStyle: .title2, compatibleWith: UITraitCollection(legibilityWeight: .bold))
+            return label
+        }()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            render()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        func render() {
+            addSubview(titleLabel)
+            NSLayoutConstraint.activate([
+                titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: CGFloat(Margin.top)),
+                titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: CGFloat(Margin.sectionTitleLeft)),
+            ])
+        }
+        
     }
 
 }
