f4dc4bfb204b07377fa378c1705fa98889d2f3cb
skycat0212 <mrmagnet777@gmail.com>
2022-10-12 01:59:04 +0900

    [Add] 컬렉션 뷰 셀 class 생성 (#8)

---
+++ CloneIKEA/CloneIKEA/Screen/Search/Cell/RecentProductCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecentProductCollectionViewCell.swift
+//  CloneIKEA
+//
+//  Created by 김수진 on 2022/10/12.
+//
+
+import UIKit
+
+class RecentProductCollectionViewCell: UICollectionViewCell {
+    
+}
