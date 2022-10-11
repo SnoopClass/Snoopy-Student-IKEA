44c02b4c251556ef764227e3eea95ad935aa27ba
skycat0212 <mrmagnet777@gmail.com>
2022-10-12 01:57:22 +0900

    [Add] 컬렉션 뷰 class 생성 (#8)

---
+++ CloneIKEA/CloneIKEA/Screen/Search/Component/RecentProcuctCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  RecentProcuctCollectionView.swift
+//  CloneIKEA
+//
+//  Created by 김수진 on 2022/10/12.
+//
+
+import UIKit
+
+class RecentProcuctCollectionView: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
