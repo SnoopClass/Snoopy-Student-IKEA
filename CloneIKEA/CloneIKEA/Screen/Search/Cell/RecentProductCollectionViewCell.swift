be2bb2b8081366fed6c3d28aca78c4ad539b3621
skycat0212 <mrmagnet777@gmail.com>
2022-10-12 02:22:01 +0900

    [Add] Cell UI 구현 (#8)

--- CloneIKEA/CloneIKEA/Screen/Search/Cell/RecentProductCollectionViewCell.swift
+++ CloneIKEA/CloneIKEA/Screen/Search/Cell/RecentProductCollectionViewCell.swift
@@ -7,6 +7,52 @@
 
 import UIKit
 
-class RecentProductCollectionViewCell: UICollectionViewCell {
+final class RecentProductCollectionViewCell: UICollectionViewCell {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    init(frame: CGRect, image: UIImage?) {
+        super.init(frame: frame)
+        setImage(image)
+        
+        render()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setImage(_ image: UIImage?) {
+        if let image = image {
+            imageView.image = image
+        }
+        imageView.image = UIImage(systemName: "book")
+    }
+    
+    private func render() {
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            
+            imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    private func configureUI() {
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = contentView.bounds.height / 2
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.lightGray.cgColor
+    }
     
 }
