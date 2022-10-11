75cfa864d20aca051720b2d27bb0dd4f84f258cf
skycat0212 <mrmagnet777@gmail.com>
2022-10-12 02:04:16 +0900

    [Add] cell 등록 (#8)

--- CloneIKEA/CloneIKEA/Screen/Search/Component/RecentProcuctCollectionView.swift
+++ CloneIKEA/CloneIKEA/Screen/Search/Component/RecentProcuctCollectionView.swift
@@ -12,10 +12,15 @@ class RecentProcuctCollectionView: UICollectionView {
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
         
+        register()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func register() {
+        register(RecentProductCollectionViewCell.self, forCellWithReuseIdentifier: RecentProductCollectionViewCell.className)
     }
 
 }
